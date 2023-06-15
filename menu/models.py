ccb231b09f146c97d298ed7b59e391661eb34a4b
Nikita Provorov <nikiyorkovich@gmail.com>
2023-06-15 22:25:37 +0300

    start new app, create models, admin

---
+++ menu/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.urls import reverse
+
+
+class TypeUser(models.Model):
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.type
+
+
+class UserAccount(AbstractUser):
+    type_user = models.ForeignKey(TypeUser, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    about = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class TypeOfDish(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    ingredients = models.TextField()
+    cooking_step = models.TextField()
+    time_to_cook = models.IntegerField()
+    type_of_dish = models.ForeignKey(TypeOfDish, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("menu:recipe-detail", kwargs={"pk": self.pk})
