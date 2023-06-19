58c1ef7c7e3f8d9e7cab845f6bee6e084053eb6a
Nikita Provorov <nikiyorkovich@gmail.com>
2023-06-19 22:07:57 +0300

    Remove Django insecure secret key

--- Fantasy/settings.py
+++ Fantasy/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO _SECRET _KEY',
-                            'django-insecure-&phwu=r(77!db%#0ab$w^dabu&^1_2kvg!!3=&y8!pv-m*94!#')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ. get('DJANGO_DEBUG', '') != 'False'
 
